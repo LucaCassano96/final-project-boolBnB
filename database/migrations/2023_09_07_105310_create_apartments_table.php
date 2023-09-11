22a33304e6037d319649f2ede9189138c618dc76
CarloSalaris <carloa.salaris@gmail.com>
2023-09-12 00:32:55 +0200

    Apartments migration city -> NULL, latitude + longitude NOT null

--- database/migrations/2023_09_07_105310_create_apartments_table.php
+++ database/migrations/2023_09_07_105310_create_apartments_table.php
@@ -25,10 +25,10 @@ return new class extends Migration
             $table -> integer("square_meters");
             $table -> text("picture");
             $table -> integer("price");
-            $table -> string("city");
+            $table -> string("city") -> nullable();
             $table -> text("address");
-            $table -> float("latitude") -> nullable();
-            $table -> float("longitude") -> nullable();
+            $table -> float("latitude") /* -> nullable() */;
+            $table -> float("longitude") /* -> nullable() */;
 
             $table -> boolean("visible") -> default(true);
 
