2600b35510a5dfe90eb64878f57678de5d9a1ddc
Bufal8 <125837797+Bufal8@users.noreply.github.com>
2023-09-15 16:44:43 +0200

    Risoluzione problema classi elementi dashboard

--- resources/views/messageApartment.blade.php
+++ resources/views/messageApartment.blade.php
@@ -1,33 +1,35 @@
 @extends('layouts.app')
 @section('content')
 
-@foreach ($users as $user)
-    @if ($user->id === Auth::user()->id)
+    @foreach ($users as $user)
 
+        @if ($user->id === Auth::user()->id)
 
+            {{-- CARD APPARTAMENTO --}}
+            <div class="apartment border-white p-3 m-auto d-flex" style="flex-direction:row;">
 
-        {{-- CARD APPARTAMENTO --}}
-        <div class="apartment border-white p-3 m-auto d-flex" style="flex-direction:row;">
+                @foreach ($user->apartments as $apartment)
 
-            @foreach ($user->apartments as $apartment)
+                    <div class="card border m-3 p-2" style="background-color: #e0a458; max-width:300px">
+                        <h3 class="text-uppercase fst-italic">
+                            <a href="{{ route('apartment.show', $apartment->id) }}" class="text-black text-decoration-none">{{ $apartment->title }}</a>
+                        </h3>
 
-            <div class="card border m-3 p-2" style="background-color: #e0a458; max-width:300px">
-                <h3 class="text-uppercase fst-italic">
-                    <a href="{{ route('apartment.show', $apartment->id) }}" class="text-black text-decoration-none">{{ $apartment->title }}</a>
-                </h3>
+                        <span>
+                            Prezzo: {{ $apartment->price }}€ / notte
+                        </span>
 
-                <span>
-                    Prezzo: {{ $apartment->price }}€ / notte
-                </span>
+                        <div class="picture mt-3" style="max-width:275px; height:200px">
+                            <img class="rounded" style="max-width: 100%" src="{{asset('storage/' . $apartment -> picture) }}" alt="{{ $apartment->title }}">
+                        </div>
+                    </div>
 
-                <div class="picture mt-3" style="max-width:275px; height:200px">
-                    <img class="rounded" style="max-width: 100%" src="{{asset('storage/' . $apartment -> picture) }}" alt="{{ $apartment->title }}">
-                </div>
+                @endforeach
+
             </div>
 
-        @endforeach
-    </div>
-@endif
-@endforeach
+
+        @endif
+    @endforeach
 
 @endsection
