a21f075e64ba380d1abb98a5c3368ce84f677c7a
CarloSalaris <carloa.salaris@gmail.com>
2023-09-20 18:39:54 +0200

    Fixed messageApartment

--- resources/views/messageApartment.blade.php
+++ resources/views/messageApartment.blade.php
@@ -1,8 +1,8 @@
 @extends('layouts.app')
 @section('content')
 
-<div class="container-fluid p-5"  style="background-color: #2d3047;">
-    <div class="card m-5 mb-0 rounded" style="background-color: #5c80bc">
+<div class="container-fluid container-lg p-3"  style="background-color: #2d3047;">
+    <div class="card rounded" style="background-color: #5c80bc">
 
         {{-- SEZIONE CARD-HEADER --}}
         <div class="card-header">
@@ -35,67 +35,62 @@
             </div>
         </div>
 
-        @foreach ($users as $user)
-
-            @if ($user->id === Auth::user()->id)
-                <div class=" p-3  flex-row">
-                    @foreach ($user->apartments as $apartment)
-
-                        {{-- CARD APPARTAMENTO + MESSAGGIO --}}
-                            <div class="card card-messaggio p-3 mb-3 d-flex flex-row">
-
-                                {{-- APPARTMENTO --}}
-                                <div class="p-2 rounded" style="background-color: #e0a458; max-width:200px">
-                                    <h5 class="text-uppercase fst-italic inline">
-                                        <a href="{{ route('apartment.show', $apartment->id) }}" class="text-black text-decoration-none">{{$apartment->title}}</a>
-                                    </h5>
-                                    <span class="d-block">
-                                        Prezzo: {{$apartment->price}}€ / notte
-                                    </span>
-                                    <div class="rounded" style="width:100%; aspect-ratio: 16 / 10;">
-                                        <img class="rounded" src="{{asset('storage/' . $apartment -> picture) }}" alt="{{ $apartment->title }}" style="width: 100%; height: 100%; object-fit: cover;">
-                                    </div>
-                                </div>
-
-                                @foreach ($apartment->messages as $message)
-                                    {{-- MESSAGGIO --}}
-                                    <div class="p-2 d-flex justify-content-between" style="width: 80%">
-                                        <div class="text">
-
-                                            <div class="dati-mittente d-flex">
-                                                <div class="mb-3 px-3">
-                                                    <span  class="d-block fw-bold">Email Mittente</span>
-                                                    <a class="inviatore" href="mailto:{{$message->sender_email}}">{{$message->sender_email}}</a>
-                                                </div>
-                                                <div class="mb-3 px-3">
-                                                    <span class="d-block fw-bold">Dati Mittente</span>
-                                                    {{$message->sender_name}} {{$message->sender_surname}}
-                                                </div>
-                                            </div>
-                                            <div class="mb-3 px-3 d-flex" >
-                                                <p class="d-inline-flex gap-1 mx-2">
-                                                    <button class="btn btn-primary" type="button" data-bs-toggle="collapse" data-bs-target="#collapse-{{$message->id}}" aria-expanded="false" aria-controls="collapse" style="height: 40px">
-                                                    Messaggio
-                                                    </button>
-                                                </p>
-                                                <div class="collapse" id="collapse-{{$message->id}}">
-                                                    <div class="card card-body overflow-auto text-dark">
-                                                    {{$message->content}}
-                                                    </div>
+        <div class="card-body">
+            @foreach ($users as $user)
+                @if ($user->id === Auth::user()->id)
+                    <div class="row">
+                        @foreach ($user->apartments as $apartment)
+                            {{-- CARD APPARTAMENTO + MESSAGGIO --}}
+                                <div class="card bg-transparent border border-warning border-3 mb-3 p-3">
+                                    <div class="row">
+                                        {{-- APPARTMENTO --}}
+                                        <div class="col-12 col-sm-6 col-md-3 d-flex align-items-center justify-content-center justify-content-sm-start">
+                                            <div class="p-2 rounded" style="background-color: #e0a458; max-width:200px">
+                                                <h5 class="text-uppercase fst-italic inline">
+                                                    <a href="{{ route('apartment.show', $apartment->id) }}" class="text-black text-decoration-none">{{$apartment->title}}</a>
+                                                </h5>
+                                                <div class="rounded" style="width:100%; aspect-ratio: 16 / 10;">
+                                                    <img class="rounded" src="{{asset('storage/' . $apartment -> picture) }}" alt="{{ $apartment->title }}" style="width: 100%; height: 100%; object-fit: cover;">
                                                 </div>
                                             </div>
                                         </div>
+                                        @foreach ($apartment->messages as $message)
+                                            {{-- MESSAGGIO --}}
+                                            <div class="col">
+                                                <div class="row p-3">
+                                                    <div class="col-12 col-md-5 text-center text-md-start">
+                                                        <div class="mb-3 px-3">
+                                                            <span  class="d-block fw-bold">Email Mittente</span>
+                                                            <a class="inviatore" href="mailto:{{$message->sender_email}}">{{$message->sender_email}}</a>
+                                                        </div>
+                                                        <div class="mb-3 px-3">
+                                                            <div class="d-block fw-bold">Dati Mittente</div>
+                                                            <div class="text-light">{{$message->sender_name}} {{$message->sender_surname}}</div>
+                                                        </div>
+                                                    </div>
+
+                                                    <div class="col-12 col-md-4 text-center text-md-start" >
+                                                        <p class="d-inline-flex gap-1 mx-2">
+                                                            <button class="btn btn-primary" type="button" data-bs-toggle="collapse" data-bs-target="#collapse-{{$message->id}}" aria-expanded="false" aria-controls="collapse" style="height: 40px">
+                                                            Messaggio
+                                                            </button>
+                                                        </p>
+                                                        <div class="collapse" id="collapse-{{$message->id}}">
+                                                            <div class="card card-body overflow-auto text-dark">
+                                                            {{$message->content}}
+                                                            </div>
+                                                        </div>
+                                                    </div>
+                                                </div>
+                                            </div>
+                                        @endforeach
                                     </div>
-                                @endforeach
-
-                            </div>
-
-
-                    @endforeach
-                </div>
-            @endif
-
-        @endforeach
+                                </div>
+                        @endforeach
+                    </div>
+                @endif
+            @endforeach
+        </div>
 
     </div>
 </div>
